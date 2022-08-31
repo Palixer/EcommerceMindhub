9287ce4b173a11c86ae3fb526c00190db286909e
Ibrian Festorazzi <ibrian_84@hotmail.com>
2022-08-31 12:54:22 -0300

    Agrego Product y Prepository

--- src/main/java/com/example/EcommerceMindhub/EcommerceMindhubApplication.java
+++ src/main/java/com/example/EcommerceMindhub/EcommerceMindhubApplication.java
@@ -1,7 +1,11 @@
 package com.example.EcommerceMindhub;
 
+import com.example.EcommerceMindhub.repositories.ClientRepository;
+import com.example.EcommerceMindhub.repositories.ShoppingCartRepositorie;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EcommerceMindhubApplication {
@@ -9,5 +13,16 @@ public class EcommerceMindhubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceMindhubApplication.class, args);
 	}
+
+
+
+	@Bean
+	//Instanciamos el repositorio
+	public CommandLineRunner initData(ClientRepository clientRepository,
+									  ShoppingCartRepositorie shoppingCartRepositories)  {
+		return (args) ->{
+
+		};
+	};
 
 }
