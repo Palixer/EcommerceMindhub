015fc81724348255d51a10aba5548bc31b85b29e
lucia.saederup <luciasaederup@gmail.com>
2022-08-31 11:10:57 -0300

    Initial commit

---
+++ src/main/java/com/example/EcommerceMindhub/EcommerceMindhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceMindhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMindhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMindhubApplication.class, args);
+	}
+
+}
