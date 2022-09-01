d1d504b039470fbb70160f4ab8dce9e045af4b06
Ibrian Festorazzi <ibrian_84@hotmail.com>
2022-09-01 09:03:21 -0300

    Cambio nombre de shoppingCartRepo y lo cambio en homeapp

--- src/main/java/com/example/EcommerceMindhub/EcommerceMindhubApplication.java
+++ src/main/java/com/example/EcommerceMindhub/EcommerceMindhubApplication.java
@@ -1,7 +1,7 @@
 package com.example.EcommerceMindhub;
 
 import com.example.EcommerceMindhub.repositories.ClientRepository;
-import com.example.EcommerceMindhub.repositories.ShoppingCartRepositorie;
+import com.example.EcommerceMindhub.repositories.ShoppingCartRepository;
 import com.example.EcommerceMindhub.repositories.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,11 +20,11 @@ public class EcommerceMindhubApplication {
 	@Bean
 	//Instanciamos el repositorio
 	public CommandLineRunner initData(ClientRepository clientRepository,
-									  ShoppingCartRepositorie shoppingCartRepositories,
+									  ShoppingCartRepository shoppingCartRepository,
 									  ProductRepository productRepository)  {
 		return (args) ->{
 
 		};
-	};
+	}
 
 }
